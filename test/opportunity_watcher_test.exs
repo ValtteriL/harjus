9f54ede0e6f20e3176c9bec6e6d0ae9f41218673
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-07 22:52:39 +0200

    start ow tests

--- test/opportunity_watcher_test.exs
+++ test/opportunity_watcher_test.exs
@@ -2,15 +2,25 @@ defmodule OpportunityWatcherTest do
   use ExUnit.Case, async: true
   doctest OpportunityWatcher
 
-  # setup do
-  #  {:ok, bucket} = KV.Bucket.start_link([])
-  #  %{bucket: bucket}
-  # end
-  #
-  # test "symbol update emits positive opportunities" do
-  #  assert 1 == 2
-  # end
-  # test "symbol update does not emit negative opportunities" do
-  #  assert 1 == 2
-  # end
+  setup do
+    # TODO: add trading paths?
+    # TODO: pass pid where to send opportunities here?
+    {:ok, pid} = OpportunityWatcher.start_link([])
+    %{pid: pid}
+  end
+
+  test "symbol update emits positive opportunities", %{pid: pid} do
+    # TODO
+    OpportunityWatcher.update_symbol(pid, {"BTCUSDT", 10000.0, 1.0})
+  end
+
+  test "symbol update does not emit negative opportunities", %{pid: pid} do
+    # TODO
+    OpportunityWatcher.update_symbol(pid, {"BTCUSDT", 10000.0, 1.0})
+  end
+
+  test "no opportunities emitted if uninitialized", %{pid: pid} do
+    # TODO
+    OpportunityWatcher.update_symbol(pid, {"BTCUSDT", 10000.0, 1.0})
+  end
 end
