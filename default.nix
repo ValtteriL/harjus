96f905dc5d93353a2861775932cddab69e88ed2a
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-12 22:47:55 +0200

    start configuring nix build

--- default.nix
+++ default.nix
@@ -10,6 +10,29 @@ with pkgs;
 let
   packages = rec {
 
+    # the derivation for kirnu
+
+    test = beamPackages.mixRelease {
+      pname = "kirnu";
+      version = "1.0.0";
+      src = ./.;
+      removecookie = false;
+      mixNixDeps = with pkgs; import ./deps.nix {
+        inherit lib beamPackages;
+        overrides = (self: super: {
+          kirnu = beamPackages.buildMix {
+            name = "kirnu";
+            version = "1.0.0";
+            src = ./.;
+            beamDeps = [];
+          };
+        });
+      };
+      postInstall = ''
+    echo "DONE!"
+  	'';
+    };
+
     # The shell of our experiment runtime environment
     devEnv = mkShellNoCC rec {
       name = "devEnv";
@@ -31,6 +54,8 @@ let
         cowsay "Kirnu!"
       '';
     };
+
+
 
   };
 in
---
+++ deps.nix
@@ -0,0 +1,194 @@
+{ lib, beamPackages, overrides ? (x: y: {}) }:
+
+let
+  buildRebar3 = lib.makeOverridable beamPackages.buildRebar3;
+  buildMix = lib.makeOverridable beamPackages.buildMix;
+  buildErlangMk = lib.makeOverridable beamPackages.buildErlangMk;
+
+  self = packages // (overrides self packages);
+
+  packages = with beamPackages; with self; {
+    dialyxir = buildMix rec {
+      name = "dialyxir";
+      version = "1.4.5";
+
+      src = fetchHex {
+        pkg = "dialyxir";
+        version = "${version}";
+        sha256 = "b0fb08bb8107c750db5c0b324fa2df5ceaa0f9307690ee3c1f6ba5b9eb5d35c3";
+      };
+
+      beamDeps = [ erlex ];
+    };
+
+    dotenv_parser = buildMix rec {
+      name = "dotenv_parser";
+      version = "2.0.1";
+
+      src = fetchHex {
+        pkg = "dotenv_parser";
+        version = "${version}";
+        sha256 = "f00780ad69e9089bd9b782e88d3e8110141dff0c54577b6c3735cef6925e4272";
+      };
+
+      beamDeps = [];
+    };
+
+    erlex = buildMix rec {
+      name = "erlex";
+      version = "0.2.7";
+
+      src = fetchHex {
+        pkg = "erlex";
+        version = "${version}";
+        sha256 = "3ed95f79d1a844c3f6bf0cea61e0d5612a42ce56da9c03f01df538685365efb0";
+      };
+
+      beamDeps = [];
+    };
+
+    finch = buildMix rec {
+      name = "finch";
+      version = "0.19.0";
+
+      src = fetchHex {
+        pkg = "finch";
+        version = "${version}";
+        sha256 = "fc5324ce209125d1e2fa0fcd2634601c52a787aff1cd33ee833664a5af4ea2b6";
+      };
+
+      beamDeps = [ mime mint nimble_options nimble_pool telemetry ];
+    };
+
+    hpax = buildMix rec {
+      name = "hpax";
+      version = "1.0.0";
+
+      src = fetchHex {
+        pkg = "hpax";
+        version = "${version}";
+        sha256 = "7f1314731d711e2ca5fdc7fd361296593fc2542570b3105595bb0bc6d0fad601";
+      };
+
+      beamDeps = [];
+    };
+
+    jason = buildMix rec {
+      name = "jason";
+      version = "1.4.4";
+
+      src = fetchHex {
+        pkg = "jason";
+        version = "${version}";
+        sha256 = "c5eb0cab91f094599f94d55bc63409236a8ec69a21a67814529e8d5f6cc90b3b";
+      };
+
+      beamDeps = [];
+    };
+
+    mime = buildMix rec {
+      name = "mime";
+      version = "2.0.6";
+
+      src = fetchHex {
+        pkg = "mime";
+        version = "${version}";
+        sha256 = "c9945363a6b26d747389aac3643f8e0e09d30499a138ad64fe8fd1d13d9b153e";
+      };
+
+      beamDeps = [];
+    };
+
+    mint = buildMix rec {
+      name = "mint";
+      version = "1.6.2";
+
+      src = fetchHex {
+        pkg = "mint";
+        version = "${version}";
+        sha256 = "5ee441dffc1892f1ae59127f74afe8fd82fda6587794278d924e4d90ea3d63f9";
+      };
+
+      beamDeps = [ hpax ];
+    };
+
+    nimble_options = buildMix rec {
+      name = "nimble_options";
+      version = "1.1.1";
+
+      src = fetchHex {
+        pkg = "nimble_options";
+        version = "${version}";
+        sha256 = "821b2470ca9442c4b6984882fe9bb0389371b8ddec4d45a9504f00a66f650b44";
+      };
+
+      beamDeps = [];
+    };
+
+    nimble_pool = buildMix rec {
+      name = "nimble_pool";
+      version = "1.1.0";
+
+      src = fetchHex {
+        pkg = "nimble_pool";
+        version = "${version}";
+        sha256 = "af2e4e6b34197db81f7aad230c1118eac993acc0dae6bc83bac0126d4ae0813a";
+      };
+
+      beamDeps = [];
+    };
+
+    poison = buildMix rec {
+      name = "poison";
+      version = "6.0.0";
+
+      src = fetchHex {
+        pkg = "poison";
+        version = "${version}";
+        sha256 = "bb9064632b94775a3964642d6a78281c07b7be1319e0016e1643790704e739a2";
+      };
+
+      beamDeps = [];
+    };
+
+    req = buildMix rec {
+      name = "req";
+      version = "0.5.8";
+
+      src = fetchHex {
+        pkg = "req";
+        version = "${version}";
+        sha256 = "d7fc5898a566477e174f26887821a3c5082b243885520ee4b45555f5d53f40ef";
+      };
+
+      beamDeps = [ finch jason mime ];
+    };
+
+    telemetry = buildRebar3 rec {
+      name = "telemetry";
+      version = "1.3.0";
+
+      src = fetchHex {
+        pkg = "telemetry";
+        version = "${version}";
+        sha256 = "7015fc8919dbe63764f4b4b87a95b7c0996bd539e0d499be6ec9d7f3875b79e6";
+      };
+
+      beamDeps = [];
+    };
+
+    websockex = buildMix rec {
+      name = "websockex";
+      version = "0.4.3";
+
+      src = fetchHex {
+        pkg = "websockex";
+        version = "${version}";
+        sha256 = "95f2e7072b85a3a4cc385602d42115b73ce0b74a9121d0d6dbbf557645ac53e4";
+      };
+
+      beamDeps = [];
+    };
+  };
+in self
+
