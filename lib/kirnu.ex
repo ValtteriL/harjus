9eed7f29b791d855751508da3e8cc5e72b9a3a18
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-17 22:14:29 +0200

    rename application file

--- lib/kirnu.ex
+++ lib/kirnu.ex
@@ -1,4 +1,4 @@
-defmodule Kirnu.Application do
+defmodule Kirnu do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Kirnu.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Kirnu.Application, []}
+      mod: {Kirnu, []}
     ]
   end
 
