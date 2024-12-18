417a1251d4ffcde44dc6674bf3caa187a1518add
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-18 10:52:21 +0200

    use testnet by default

--- lib/binance.ex
+++ lib/binance.ex
@@ -1,8 +1,10 @@
 defmodule Binance do
   # Get all trading pairs from Binance
-  @spec get_symbols() :: [%{symbol: charlist(), baseAsset: charlist(), quoteAsset: charlist()}]
-  def get_symbols do
-    {:ok, resp} = Req.get("https://api.binance.com/api/v3/exchangeInfo")
+  @spec get_symbols(url :: charlist()) :: [
+          %{symbol: charlist(), baseAsset: charlist(), quoteAsset: charlist()}
+        ]
+  def get_symbols(url \\ "https://testnet.binance.vision/api/v3/exchangeInfo") do
+    {:ok, resp} = Req.get(url)
 
     resp.body["symbols"]
     |> Enum.map(fn x ->
