b7ac8ac22396bb09dbf5d4a213ff64fb1394d60a
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-18 14:34:13 +0200

    reduce default path length limit

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,7 @@ end
 
 # Now variables from `.env` are loaded into system env
 config :harjus,
-  max_trading_path_length: ConfigHelper.get_env("MAX_TRADING_PATH_LENGTH", 4, :int),
+  max_trading_path_length: ConfigHelper.get_env("MAX_TRADING_PATH_LENGTH", 2, :int),
   start_symbols: ConfigHelper.get_env("START_SYMBOLS", [], :list),
   min_profit_percentage: ConfigHelper.get_env("MIN_PROFIT_PERCENTAGE", 0.001, :float),
   min_capacity: ConfigHelper.get_env("MIN_CAPACITY", 0.0, :float),
