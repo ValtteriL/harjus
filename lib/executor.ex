5e204260129cf66f0a678c90d42ae0144be78df4
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-22 22:09:27 +0200

    WIP: start implementing logic to PM
    also create dummy executor

---
+++ lib/executor.ex
@@ -0,0 +1,90 @@
+defmodule Executor do
+  @moduledoc """
+  Process for executing trades.
+
+  Gets approved and prioritized opportunities from PortfolioManager,
+  and executes them.
+
+  While executing a trade, discards all new opportunities.
+  """
+
+  # maybe statem more appropriate?
+  use GenServer
+  require Logger
+
+  # TODO
+  @type state() :: %{
+          pid: pid()
+        }
+
+  @type trading_symbol() :: {charlist(), :long | :short}
+  @type opportunity() :: {path :: [trading_symbol()], profit :: float(), capacity :: float()}
+
+  # API
+
+  @doc """
+  Start the executor
+
+  """
+  @spec start_link(arg :: any()) ::
+          :ignore | {:error, any()} | {:ok, pid()}
+  def start_link(_arg) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @doc """
+  Send opportunities to Portfolio Manager
+  """
+  @spec send_opportunities(
+          pid :: pid(),
+          opportunities :: [
+            opportunity()
+          ]
+        ) ::
+          :ok
+  def send_opportunities(pid, opportunities) do
+    GenServer.cast(pid, {:update_opportunities, opportunities})
+  end
+
+  # Callbacks
+
+  @impl true
+  @spec init({pid()}) ::
+          {:ok,
+           %{
+             pid: pid()
+           }}
+  def init(pid) do
+    initial_state = %{
+      pid: pid
+    }
+
+    {:ok, initial_state}
+  end
+
+  @impl true
+  @spec handle_cast(
+          {:update_opportunities, [opportunity()]},
+          %{
+            pid: pid()
+          }
+        ) ::
+          {:noreply,
+           %{
+             pid: pid()
+           }}
+  def handle_cast(
+        {:update_opportunities, opportunities},
+        state
+      ) do
+    # update state
+    new_state = %{
+      pid: state.pid
+    }
+
+    # TODO
+    Logger.notice("Portfolio Manager: received opportunities #{inspect(opportunities)}")
+
+    {:noreply, new_state}
+  end
+end
--- lib/portfolio_manager.ex
+++ lib/portfolio_manager.ex
@@ -10,6 +10,23 @@ defmodule PortfolioManager do
   use GenServer
   require Logger
 
+  @type pm_args() :: %{
+          min_profit_percentage: float(),
+          min_profit_capacity: float(),
+          standard_commission: float(),
+          tax_commission: float(),
+          discount: float()
+        }
+  @type state() :: %{
+          # executor pid
+          pid: pid(),
+          min_profit_percentage: float(),
+          min_profit_capacity: float(),
+          standard_commission: float(),
+          tax_commission: float(),
+          discount: float()
+        }
+
   @type trading_symbol() :: {charlist(), :long | :short}
   @type opportunity() :: {path :: [trading_symbol()], profit :: float(), capacity :: float()}
 
@@ -19,14 +36,15 @@ defmodule PortfolioManager do
   Start the portfolio manager
 
   Args:
-    pid: pid of the process to send the most profitable opportunities
-    TODO: here would be place for trading fees, info which symbols are in margin, ...
+    min_profit_percentage : minimum profit percentage to consider an opportunity
+    min_profit_capacity : minimum capacity to consider an opportunity
+    standard_commission : standard trading commission
+    tax_commission : trading tax commission
+    discount : discount rate on standard commission if paying fees with BNB
   """
-  @spec start_link(arg :: any()) :: :ignore | {:error, any()} | {:ok, pid()}
-  def start_link(_arg) do
-    # TODO: put executor here
-    pid = nil
-    GenServer.start_link(__MODULE__, pid, name: __MODULE__)
+  @spec start_link(args :: pm_args()) :: :ignore | {:error, any()} | {:ok, pid()}
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   @doc """
@@ -46,14 +64,18 @@ defmodule PortfolioManager do
   # Callbacks
 
   @impl true
-  @spec init({pid()}) ::
-          {:ok,
-           %{
-             pid: pid()
-           }}
-  def init(pid) do
+  @spec init(args :: pm_args()) ::
+          {:ok, state()}
+  def init(args) do
+    pid = Process.whereis(Executor)
+
     initial_state = %{
-      pid: pid
+      pid: pid,
+      min_profit_percentage: args.min_profit_percentage,
+      min_profit_capacity: args.min_profit_capacity,
+      standard_commission: args.standard_commission,
+      tax_commission: args.tax_commission,
+      discount: args.discount
     }
 
     {:ok, initial_state}
@@ -62,26 +84,16 @@ defmodule PortfolioManager do
   @impl true
   @spec handle_cast(
           {:update_opportunities, [opportunity()]},
-          %{
-            pid: pid()
-          }
+          state()
         ) ::
-          {:noreply,
-           %{
-             pid: pid()
-           }}
+          {:noreply, state()}
   def handle_cast(
         {:update_opportunities, opportunities},
         state
       ) do
-    # update state
-    new_state = %{
-      pid: state.pid
-    }
-
     # TODO
     Logger.notice("Portfolio Manager: received opportunities #{inspect(opportunities)}")
 
-    {:noreply, new_state}
+    {:noreply, state}
   end
 end
