fbfca5ec26aee41abfb2243d792f7c9f75f00561
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-23 23:03:50 +0200

    Add credo
    `mix credo` runs style consistency analysi

--- mix.exs
+++ mix.exs
@@ -35,10 +35,17 @@ defmodule Harjus.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:castore, "~> 1.0"},
 <<<<<<< HEAD
+<<<<<<< HEAD
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
 =======
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
 >>>>>>> 18e3a13 (Add mix task quality for code quality checks)
+=======
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+=======
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+>>>>>>> 94d18a0 (Add credo)
+>>>>>>> 8b1c180 (Add credo)
     ]
   end
 end
