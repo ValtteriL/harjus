ecdd81ba0341465a0109b44824526cbcb39c4629
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-12-29 21:05:30 +0200

    improve readability

--- lib/config_helper.ex
+++ lib/config_helper.ex
@@ -17,9 +17,8 @@ defmodule ConfigHelper do
   end
 
   def get_env(var, default, type) do
-    with {:ok, val} <- System.fetch_env(var) do
-      get_with_type(val, type)
-    else
+    case System.fetch_env(var) do
+      {:ok, val} -> get_with_type(val, type)
       :error -> default
     end
   end
