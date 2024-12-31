2c5aa19c47d44c1cd95f107306926797d18044e9
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-01 00:06:05 +0200

    start using existing functions for serialization

--- lib/binance_fix_api.ex
+++ lib/binance_fix_api.ex
@@ -7,30 +7,49 @@ defmodule BinanceFixApi do
 
   @type trading_symbol() :: {charlist(), :long | :short}
 
-  # InMessage types
-  @msg_type_heartbeat "0"
-  @soh_character 1
-
-  def logon(public_key, private_key) do
+  defmodule MessageToSend do
+    @moduledoc "serializer argument map"
+    @type t :: %MessageToSend{}
+    defstruct seqnum: 0,
+              msg_type: nil,
+              sender: nil,
+              orig_sending_time: nil,
+              extra_header: [],
+              body: []
   end
 
-  def market_order_request(trading_symbol, quantity) do
+  # InMessage types
+  @msg_type_heartbeat "0"
+  @tag_msg_type "35"
+  @tag_seqnum "34"
+  @tag_sender_comp_id "49"
+  @tag_sending_time "52"
+  @tag_target_comp_id "56"
+  @tag_poss_dup_flag "43"
+  @tag_orig_sending_time "122"
+
+  # dummy defaults
+  @sender "123"
+
+  def logon(seq_num, public_key, private_key) do
     # TODO
   end
 
-  def heartbeat do
-    timestamp = timestamp()
-    seq_num = 1
+  def market_order_request(seq_num, trading_symbol, quantity) do
+    # TODO
+  end
 
-    header =
-      <<"8=FIX.4.4", @soh_character, "9=", bin_len::binary, @soh_character, "35=",
-        @msg_type_heartbeat, @soh_character, "49=123", @soh_character, "56=SPOT", @soh_character,
-        "34=", seq_num::binary, @soh_character, "52=", timestamp::binary, @soh_character>>
-
-    checksum = calculate_checksum(header)
-    trailer = <<"10=", checksum::binary, @soh_character>>
-
-    <<header::binary, body::binary, trailer::binary>>
+  def heartbeat(seq_num) do
+    serialize(
+      %MessageToSend{
+        seqnum: seq_num,
+        msg_type: @msg_type_heartbeat,
+        sender: @sender,
+        orig_sending_time: nil,
+        body: []
+      },
+      timestamp()
+    )
   end
 
   def parse_message(message) do
@@ -42,7 +61,96 @@ defmodule BinanceFixApi do
     Calendar.strftime(DateTime.utc_now(), "%Y%m%d-%H:%M:%S.%f")
   end
 
-  defp calculate_checksum(header) do
-    # TODO
+  @spec serialize(MessageToSend.t(), DateTime.t(), boolean()) :: binary()
+  defp serialize(
+         %MessageToSend{
+           seqnum: seqnum,
+           msg_type: msg_type,
+           sender: sender,
+           orig_sending_time: orig_sending_time,
+           extra_header: extra_header,
+           body: body
+         },
+         sending_time,
+         resend \\ false
+       ) do
+    header =
+      case resend do
+        false ->
+          [
+            {@tag_sender_comp_id, sender},
+            {@tag_sending_time, sending_time},
+            {@tag_target_comp_id, "SPOT"}
+          ]
+
+        true ->
+          [
+            {@tag_sender_comp_id, sender},
+            {@tag_poss_dup_flag, true},
+            {@tag_sending_time, sending_time},
+            {@tag_orig_sending_time, orig_sending_time},
+            {@tag_target_comp_id, "SPOT"}
+          ]
+      end
+
+    fields = header ++ extra_header ++ body
+
+    {:ok, body, bin_len, cs_total} =
+      fields_to_bin([{@tag_msg_type, msg_type}, {@tag_seqnum, seqnum} | fields])
+
+    head = <<"8=FIX.4.4", 1, "9=", bin_len::binary, 1>>
+    checksum_bin = calculate_checksum(cs_total, head)
+    <<head::binary, body::binary, "10=", checksum_bin::binary, 1>>
+  end
+
+  defp fields_to_bin(fields), do: fields_to_bin(fields, [], 0, 0)
+
+  defp fields_to_bin([], bin, len, cs_total) do
+    result =
+      bin
+      |> Enum.reverse()
+      |> IO.iodata_to_binary()
+
+    {:ok, result, "#{len}", cs_total}
+  end
+
+  defp fields_to_bin([{tag, value} | rest], bin, len, cstot) do
+    bin_value = serialize_value(value)
+    pair = <<tag::binary, "=", bin_value::binary, 1>>
+    fields_to_bin(rest, [pair | bin], len + byte_size(pair), bin_sum(pair, cstot))
+  end
+
+  defp serialize_value(v) when is_binary(v) do
+    :unicode.characters_to_binary(v, :utf8, :latin1)
+  end
+
+  defp serialize_value(v) when is_float(v) do
+    :erlang.float_to_binary(v, [{:decimals, 10}, :compact])
+  end
+
+  defp serialize_value(%DateTime{} = v) do
+    DateUtil.serialize_date(v)
+  end
+
+  defp serialize_value(v) when is_integer(v), do: Integer.to_string(v)
+  defp serialize_value(true), do: "Y"
+  defp serialize_value(false), do: "N"
+  defp serialize_value(v) when is_atom(v), do: Atom.to_string(v)
+  defp serialize_value(nil), do: ""
+
+  def bin_sum(<<>>, acc), do: acc
+
+  def bin_sum(<<value::binary-size(1), rest::binary>>, acc) do
+    bin_sum(rest, acc + :binary.decode_unsigned(value))
+  end
+
+  defp calculate_checksum(cs_total, extra_bytes) do
+    checksum = rem(bin_sum(extra_bytes, cs_total), 256)
+
+    case checksum do
+      cs when cs < 10 -> "00#{cs}"
+      cs when cs < 100 -> "0#{cs}"
+      cs -> "#{cs}"
+    end
   end
 end
---
+++ lib/date_util.ex
@@ -0,0 +1,55 @@
+defmodule DateUtil do
+  @moduledoc """
+  FIX DateTime related functions
+  """
+  alias Calendar.DateTime.Format
+
+  @compile {:inline, pad2: 1, pad3: 1}
+
+  def serialize_date(%DateTime{
+        calendar: Calendar.ISO,
+        day: day,
+        hour: hour,
+        microsecond: {micro, _},
+        minute: minute,
+        month: month,
+        second: second,
+        std_offset: 0,
+        time_zone: "Etc/UTC",
+        utc_offset: 0,
+        year: year
+      }) do
+    year_bin = "#{year}"
+    month_bin = pad2(month)
+    day_bin = pad2(day)
+    hour_bin = pad2(hour)
+    minute_bin = pad2(minute)
+    second_bin = pad2(second)
+    millis_bin = pad3(div(micro, 1000))
+
+    <<year_bin::binary, month_bin::binary, day_bin::binary, "-", hour_bin::binary, ":",
+      minute_bin::binary, ":", second_bin::binary, ".", millis_bin::binary>>
+  end
+
+  def serialize_date(%DateTime{} = date_time) do
+    <<yyyy::binary-size(4), "-", mm::binary-size(2), "-", dd::binary-size(2), "T",
+      time::binary-size(12),
+      _rest::binary>> =
+      date_time
+      |> Calendar.DateTime.shift_zone!("Etc/UTC")
+      |> Format.rfc3339(3)
+
+    <<yyyy::binary, mm::binary, dd::binary, "-", time::binary>>
+  end
+
+  ##
+  ## Private functions
+  ##
+
+  defp pad2(num) when num < 10, do: "0#{num}"
+  defp pad2(num), do: "#{num}"
+
+  defp pad3(num) when num < 10, do: "00#{num}"
+  defp pad3(num) when num < 100, do: "0#{num}"
+  defp pad3(num), do: "#{num}"
+end
