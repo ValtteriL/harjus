a14f542fd61edc2c3cbde9759be43296d6974d6f
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-02 22:21:33 +0200

    add dummy tests for fix api

---
+++ test/binance_fix_api_test.exs
@@ -0,0 +1,45 @@
+defmodule BinanceFixApiTest do
+  @moduledoc "Tests for BinanceFixApi"
+  use ExUnit.Case
+  doctest BinanceFixApi
+
+  test "generates correct heartbeat message" do
+    assert 1 == 0
+  end
+
+  test "generates correct logon request" do
+    assert 1 == 0
+  end
+
+  test "generates correct market order request" do
+    assert 1 == 0
+  end
+
+  test "parses heartbeat message" do
+    assert 1 == 0
+  end
+
+  test "parses test_request message" do
+    assert 1 == 0
+  end
+
+  test "parses reject message" do
+    assert 1 == 0
+  end
+
+  test "parses logon message" do
+    assert 1 == 0
+  end
+
+  test "parses news message" do
+    assert 1 == 0
+  end
+
+  test "parses execution_report message" do
+    assert 1 == 0
+  end
+
+  test "parses unknown message" do
+    assert 1 == 0
+  end
+end
