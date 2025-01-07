fe47173d436e7271f95b0ce0071752acaac2de7d
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-07 22:40:03 +0200

    update elixir to 1.8.1

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import
     (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
-      sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+      url = "https://github.com/NixOS/nixpkgs/archive/b30f97d8c32d804d2d832ee837d0f1ca0695faa5.tar.gz";
+      sha256 = "sha256:11af9sclvfq1477khps9g9anra5x2xnhl7527c99j1ld01wg33jk";
     })
     { }
 }:
