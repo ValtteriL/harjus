f98d9e32105c28b3ab5955bcc61fe535c633876a
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-23 21:35:46 +0200

    implement metric reporting functions

--- lib/metrics.ex
+++ lib/metrics.ex
@@ -9,4 +9,28 @@ defmodule Metrics do
           | Telemetry.Metrics.LastValue.t()
         ]
   defdelegate metrics(), to: Impl
+
+  @spec report_trading_symbol_count(count :: integer()) :: :ok
+  defdelegate report_trading_symbol_count(count), to: Impl
+
+  @spec report_trading_path_count(count :: integer()) :: :ok
+  defdelegate report_trading_path_count(count), to: Impl
+
+  @spec report_price_update() :: :ok
+  defdelegate report_price_update(), to: Impl
+
+  @spec report_trade_executed() :: :ok
+  defdelegate report_trade_executed(), to: Impl
+
+  @spec report_trade_attempted() :: :ok
+  defdelegate report_trade_attempted(), to: Impl
+
+  @spec report_trade_losing() :: :ok
+  defdelegate report_trade_losing(), to: Impl
+
+  @spec report_trade_winning() :: :ok
+  defdelegate report_trade_winning(), to: Impl
+
+  @spec report_trade_report_delta(delta :: %{String.t() => Decimal.t()}) :: :ok
+  defdelegate report_trade_report_delta(delta), to: Impl
 end
--- lib/metrics/impl.ex
+++ lib/metrics/impl.ex
@@ -3,6 +3,18 @@ defmodule Metrics.Impl do
 
   import Telemetry.Metrics
 
+  defp trading_symbol_count, do: [:harjus, :market_data, :trading_symbol]
+  defp trading_path_count, do: [:harjus, :market_data, :trading_path]
+  defp price_update, do: [:harjus, :price_streamer, :price_update]
+  defp trade_executed, do: [:harjus, :trader, :trade, :executed]
+  defp trade_attempted, do: [:harjus, :trader, :trade, :attempted]
+  defp trade_losing, do: [:harjus, :trader, :trade, :losing]
+  defp trade_winning, do: [:harjus, :trader, :trade, :winning]
+  defp trade_report_delta, do: [:harjus, :trader, :trade, :report]
+
+  defp counter_measurement, do: :count
+  defp distribution_measurement, do: :delta
+
   @spec metrics() :: [
           Telemetry.Metrics.Counter.t()
           | Telemetry.Metrics.Summary.t()
@@ -11,24 +23,40 @@ defmodule Metrics.Impl do
   def metrics do
     [
       # market data metrics
-      last_value("harjus.market_data.trading_symbol.count",
+      last_value(trading_symbol_count(),
+        measurement: counter_measurement(),
         description: "Number of trading symbols available in exchange"
       ),
-      last_value("harjus.market_data.trading_path.count",
+      last_value(trading_path_count(),
+        measurement: counter_measurement(),
         description: "Number of possible trading paths"
       ),
 
       # price streamer metrics
-      counter("harjus.price_streamer.price_update.count",
+      counter(price_update(),
+        measurement: counter_measurement(),
         description: "Number of price updates received"
       ),
 
       # trader metrics
-      counter("harjus.trader.trade.executed.count", description: "Number of trades executed"),
-      counter("harjus.trader.trade.attempted.count", description: "Number of trades attempted"),
-      counter("harjus.trader.trade.losing.count", description: "Number of losing trades"),
-      counter("harjus.trader.trade.winning.count", description: "Number of winning trades"),
-      distribution("harjus.trader.trade.report.delta",
+      counter(trade_executed(),
+        measurement: counter_measurement(),
+        description: "Number of trades executed"
+      ),
+      counter(trade_attempted(),
+        measurement: counter_measurement(),
+        description: "Number of trades attempted"
+      ),
+      counter(trade_losing(),
+        measurement: counter_measurement(),
+        description: "Number of losing trades"
+      ),
+      counter(trade_winning(),
+        measurement: counter_measurement(),
+        description: "Number of winning trades"
+      ),
+      distribution(trade_report_delta(),
+        measurement: distribution_measurement(),
         tags: [:asset],
         description: "Change in asset balance after execution",
         reporter_options: [buckets: [0.05, 0.1, 0.2, 0.5, 1]]
@@ -40,5 +68,50 @@ defmodule Metrics.Impl do
       last_value("vm.total_run_queue_lengths.cpu"),
       last_value("vm.total_run_queue_lengths.io")
     ]
+  end
+
+  @spec report_trading_symbol_count(count :: integer()) :: :ok
+  def report_trading_symbol_count(count) do
+    :telemetry.execute(trading_symbol_count(), %{counter_measurement() => count})
+  end
+
+  @spec report_trading_path_count(count :: integer()) :: :ok
+  def report_trading_path_count(count) do
+    :telemetry.execute(trading_path_count(), %{counter_measurement() => count})
+  end
+
+  @spec report_price_update() :: :ok
+  def report_price_update do
+    :telemetry.execute(price_update(), %{counter_measurement() => 1})
+  end
+
+  @spec report_trade_executed() :: :ok
+  def report_trade_executed do
+    :telemetry.execute(trade_executed(), %{counter_measurement() => 1})
+  end
+
+  @spec report_trade_attempted() :: :ok
+  def report_trade_attempted do
+    :telemetry.execute(trade_attempted(), %{counter_measurement() => 1})
+  end
+
+  @spec report_trade_losing() :: :ok
+  def report_trade_losing do
+    :telemetry.execute(trade_losing(), %{counter_measurement() => 1})
+  end
+
+  @spec report_trade_winning() :: :ok
+  def report_trade_winning do
+    :telemetry.execute(trade_winning(), %{counter_measurement() => 1})
+  end
+
+  @spec report_trade_report_delta(delta :: %{String.t() => Decimal.t()}) :: :ok
+  def report_trade_report_delta(delta_map) do
+    delta_map
+    |> Enum.each(fn {asset, delta} ->
+      :telemetry.execute(trade_report_delta(), %{distribution_measurement() => delta}, %{
+        asset: asset
+      })
+    end)
   end
 end
