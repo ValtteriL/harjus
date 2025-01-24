d7b37798c112089c51b2e8b3426049d6cebc93fd
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-24 22:50:13 +0200

    refactor into impls

--- lib/market_data/asset_comparer.ex
+++ lib/market_data/asset_comparer.ex
@@ -5,6 +5,8 @@ defmodule MarketData.AssetComparer do
   Used by portfoliomanager to rank opportunities by value
   """
 
+  alias MarketData.AssetComparer.Impl
+
   @doc """
   calculate relative value for symbols
   """
@@ -16,30 +18,5 @@ defmodule MarketData.AssetComparer do
           comparison_asset :: String.t()
         ) ::
           %{symbol: String.t(), value: Decimal.t()}
-  def calculate_relative_values(symbols, symbol_prices, comparison_asset) do
-    # find price of every asset in comparison_asset
-
-    # where comparison_asset is the quote asset
-    btc_prices_base =
-      symbols
-      |> Enum.filter(fn x -> x.quoteAsset == comparison_asset end)
-      |> Enum.map(fn %{symbol: s, baseAsset: b} -> {b, symbol_prices[s]} end)
-      |> Enum.into(%{})
-
-    # where comparison_asset is the base asset
-    btc_prices_quote =
-      symbols
-      |> Enum.filter(fn x -> x.baseAsset == comparison_asset end)
-      |> Enum.map(fn %{symbol: s, quoteAsset: q} -> {q, 1 / symbol_prices[s]} end)
-      |> Enum.into(%{})
-
-    # merge the two maps
-    Map.merge(btc_prices_quote, btc_prices_base)
-    # add the comparison_asset itself
-    |> Map.put(comparison_asset, 1.0)
-
-    # convert floats to decimals
-    |> Enum.map(fn {k, v} -> {k, Decimal.from_float(v)} end)
-    |> Enum.into(%{})
-  end
+  defdelegate calculate_relative_values(symbols, symbol_prices, comparison_asset), to: Impl
 end
---
+++ lib/market_data/asset_comparer/impl.ex
@@ -0,0 +1,43 @@
+defmodule MarketData.AssetComparer.Impl do
+  @moduledoc """
+  AssetComparer implementation
+  """
+
+  @doc """
+  calculate relative value for symbols
+  """
+  @spec calculate_relative_values(
+          symbols :: [
+            %{symbol: String.t(), baseAsset: charlist(), quoteAsset: charlist()}
+          ],
+          symbol_prices :: %{String.t() => Decimal.t()},
+          comparison_asset :: String.t()
+        ) ::
+          %{symbol: String.t(), value: Decimal.t()}
+  def calculate_relative_values(symbols, symbol_prices, comparison_asset) do
+    # find price of every asset in comparison_asset
+
+    # where comparison_asset is the quote asset
+    btc_prices_base =
+      symbols
+      |> Enum.filter(fn x -> x.quoteAsset == comparison_asset end)
+      |> Enum.map(fn %{symbol: s, baseAsset: b} -> {b, symbol_prices[s]} end)
+      |> Enum.into(%{})
+
+    # where comparison_asset is the base asset
+    btc_prices_quote =
+      symbols
+      |> Enum.filter(fn x -> x.baseAsset == comparison_asset end)
+      |> Enum.map(fn %{symbol: s, quoteAsset: q} -> {q, 1 / symbol_prices[s]} end)
+      |> Enum.into(%{})
+
+    # merge the two maps
+    Map.merge(btc_prices_quote, btc_prices_base)
+    # add the comparison_asset itself
+    |> Map.put(comparison_asset, 1.0)
+
+    # convert floats to decimals
+    |> Enum.map(fn {k, v} -> {k, Decimal.from_float(v)} end)
+    |> Enum.into(%{})
+  end
+end
