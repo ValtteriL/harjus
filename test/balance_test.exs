0ab2df492039cec1ef6b5c51239723be0d676a85
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-25 23:03:33 +0200

    WIP: add balance property

--- test/balance_test.exs
+++ test/balance_test.exs
@@ -4,13 +4,49 @@ defmodule BalanceTest do
   """
   use ExUnit.Case
   doctest Balance
+  use PropCheck
 
   setup do
-    {:ok, pid} = Balance.start_link()
-    %{pid: pid}
+    {:ok, _pid} = Balance.start_link()
+    :ok
   end
 
-  test "updates balances correctly", %{pid: _pid} do
+  property "balance behaves correctly", [:verbose] do
+    forall [symbol, update_value] <- [non_empty_string(), float()] do
+      # starting balance is 0
+      assert Balance.get(symbol) == Decimal.new(0)
+
+      # balance is updated correctly
+      :ok = Balance.update(symbol, Decimal.from_float(update_value))
+      assert Balance.get(symbol) == Decimal.from_float(update_value)
+
+      # reserve upto the current balance
+      {reserved, _} = Balance.reserve_upto(symbol, Decimal.from_float(update_value))
+
+      # reserved amount is the same as the current balance
+      assert reserved == Decimal.from_float(update_value)
+
+      # balance is now 0
+      assert Balance.get(symbol) == Decimal.new(0)
+    end
+  end
+
+  ## Generators ##
+
+  defp non_empty_string do
+    let charlist <- non_empty(elements(textdata())) do
+      to_string(charlist)
+    end
+  end
+
+  defp textdata do
+    ~c"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" ++
+      ~c":;<=>?@ !#$%&'()*+-./[\\]^_`{|}~"
+  end
+
+  ## Unit tests ##
+
+  test "updates balances correctly" do
     symbol = "SOMETHING_COOL"
 
     # add
@@ -22,7 +58,7 @@ defmodule BalanceTest do
     assert Balance.get(symbol) == Decimal.new("25.0")
   end
 
-  test "non-existent asset returns 0", %{pid: _pid} do
+  test "non-existent asset returns 0" do
     assert Balance.get("this-does-not-exist") == Decimal.new(0)
   end
 end
