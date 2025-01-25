3e9e4c7433eccf976663b5a6ca47372076859a3b
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-01-25 22:47:35 +0200

    add guards, fix bug
    expected decimal but calculated with floats before

--- lib/market_data/asset_comparer/impl.ex
+++ lib/market_data/asset_comparer/impl.ex
@@ -14,7 +14,21 @@ defmodule MarketData.AssetComparer.Impl do
           comparison_asset :: String.t()
         ) ::
           %{symbol: String.t(), value: Decimal.t()}
-  def calculate_relative_values(symbols, symbol_prices, comparison_asset) do
+  def calculate_relative_values(
+        symbols = [%{symbol: _, baseAsset: _, quoteAsset: _} | _],
+        symbol_prices = %{},
+        # string
+        comparison_asset = "" <> _
+      ) do
+    # verify comparison_asset is base or quote asset in at least one symbols
+    false =
+      Enum.filter(symbols, fn x ->
+        x.baseAsset == comparison_asset or x.quoteAsset == comparison_asset
+      end)
+      |> Enum.empty?()
+
+    # check if comparison_asset is in symbols
+
     # find price of every asset in comparison_asset
 
     # where comparison_asset is the quote asset
@@ -28,16 +42,12 @@ defmodule MarketData.AssetComparer.Impl do
     btc_prices_quote =
       symbols
       |> Enum.filter(fn x -> x.baseAsset == comparison_asset end)
-      |> Enum.map(fn %{symbol: s, quoteAsset: q} -> {q, 1 / symbol_prices[s]} end)
+      |> Enum.map(fn %{symbol: s, quoteAsset: q} -> {q, Decimal.div(1, symbol_prices[s])} end)
       |> Enum.into(%{})
 
     # merge the two maps
     Map.merge(btc_prices_quote, btc_prices_base)
     # add the comparison_asset itself
-    |> Map.put(comparison_asset, 1.0)
-
-    # convert floats to decimals
-    |> Enum.map(fn {k, v} -> {k, Decimal.from_float(v)} end)
-    |> Enum.into(%{})
+    |> Map.put(comparison_asset, Decimal.from_float(1.0))
   end
 end
