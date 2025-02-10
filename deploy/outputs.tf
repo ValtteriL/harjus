01cbca9db6b1262338eee383d408b34eb83e3392
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-02-10 22:56:14 +0200

    output deployed image

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -3,22 +3,7 @@ output "ecr_url" {
   value       = aws_ecr_repository.ecr_repository.repository_url
 }
 
-# output "ecs_cluster_name" {
-#   description = "The name of the ECS cluster"
-#   value       = aws_ecs_cluster.qa_cluster.name
-# }
-
-# output "ecr_repository_url" {
-#   description = "The URL of the ECR repository"
-#   value       = aws_ecr_repository.qa_repository.repository_url
-# }
-
-# output "ecs_service_name" {
-#   description = "The name of the ECS service"
-#   value       = aws_ecs_service.qa_service.name
-# }
-
-# output "ecs_task_definition_arn" {
-#   description = "The ARN of the ECS task definition"
-#   value       = aws_ecs_task_definition.qa_task.arn
-# }
+output "deployed_image" {
+  description = "The container image that was deployed"
+  value       = "${aws_ecr_repository.ecr_repository.repository_url}:${var.image_tag}"
+}
