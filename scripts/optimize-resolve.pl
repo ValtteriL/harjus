ca68566b21c881b04a6e3f22c3587802ab776091
Valtteri Lehtinen <theqbot94@gmail.com>
2025-02-12 12:31:42 +0200

    Add optimize-resolve.pl script
    Add a new script `scripts/optimize-resolve.pl` to read lines of hostname:port pairs, run `nping` with `--tcp` against every IP the hostname resolves to, and hardcode the IP with the lowest average RTT in `/etc/hosts`.
    
    * Create a new Perl script file `scripts/optimize-resolve.pl`.
    * Add a shebang line to specify the Perl interpreter.
    * Add code to read lines of hostname:port pairs from standard input.
    * Add code to run `nping` with `--tcp` against every IP the hostname resolves to using backticks.
    * Add code to calculate the IP with the lowest average RTT for each hostname.
    * Add code to hardcode the IP with the lowest average RTT in `/etc/hosts`.
    * Add command line argument to specify the number of `nping` probes per target.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/ValtteriL/harjus?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ scripts/optimize-resolve.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Net::DNS;
+use IPC::Open3;
+use Symbol 'gensym';
+
+# Read number of probes from command line argument
+my $num_probes = shift @ARGV || 5;
+
+# Read hostname:port pairs from standard input
+my %hosts;
+while (<STDIN>) {
+    chomp;
+    my ($hostname, $port) = split /:/;
+    push @{$hosts{$hostname}}, $port;
+}
+
+# Resolve hostnames to IPs
+my %resolved_hosts;
+my $resolver = Net::DNS::Resolver->new;
+foreach my $hostname (keys %hosts) {
+    my $query = $resolver->search($hostname);
+    if ($query) {
+        foreach my $rr ($query->answer) {
+            next unless $rr->type eq "A";
+            push @{$resolved_hosts{$hostname}}, $rr->address;
+        }
+    }
+}
+
+# Run nping and calculate RTTs
+my %rtt_results;
+foreach my $hostname (keys %resolved_hosts) {
+    foreach my $ip (@{$resolved_hosts{$hostname}}) {
+        foreach my $port (@{$hosts{$hostname}}) {
+            my $cmd = "nping --tcp -c $num_probes $ip -p $port";
+            my $output = `$cmd`;
+            foreach my $line (split /\n/, $output) {
+                if ($line =~ /Avg rtt:\s+([\d.]+)ms/) {
+                    push @{$rtt_results{$hostname}{$ip}}, $1;
+                }
+            }
+        }
+    }
+}
+
+# Calculate lowest average RTT for each hostname
+my %best_ips;
+foreach my $hostname (keys %rtt_results) {
+    my $best_ip;
+    my $lowest_rtt = 1e9;
+    foreach my $ip (keys %{$rtt_results{$hostname}}) {
+        foreach my $rtt (@{$rtt_results{$hostname}{$ip}}) {
+            if ($rtt < $lowest_rtt) {
+                $lowest_rtt = $rtt;
+                $best_ip = $ip;
+            }
+        }
+    }
+    $best_ips{$hostname} = $best_ip;
+}
+
+# Hardcode the IPs with the lowest average RTT in /etc/hosts
+open my $hosts_fh, '>>', '/etc/hosts' or die "Could not open /etc/hosts: $!";
+foreach my $hostname (keys %best_ips) {
+    print $hosts_fh "$best_ips{$hostname} $hostname\n";
+}
+close $hosts_fh;
