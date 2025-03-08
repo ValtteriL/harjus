e91a7cded9340f50293f0853e380e217cc648fb2
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-08 21:31:14 +0200

    add commission to execution_planner signature

--- lib/pipeline/execution_planner.ex
+++ lib/pipeline/execution_planner.ex
@@ -5,5 +5,16 @@ defmodule Pipeline.TradePlanner do
 
   alias Pipeline.TradePlanner.Impl
 
-  defdelegate plan_execution(path, starting_asset_balance, relative_asset_values), to: Impl
+  @doc """
+  Generate execution plan for a given trading path, starting asset balance and relative asset values
+  """
+  @spec plan_execution([TradingSymbol.t()], Decimal.t(), Decimal.t(), map()) ::
+          PlannedExecution.t()
+  defdelegate plan_execution(
+                path,
+                starting_asset_balance,
+                commission_percentage,
+                relative_asset_values
+              ),
+              to: Impl
 end
--- lib/pipeline/execution_planner/impl.ex
+++ lib/pipeline/execution_planner/impl.ex
@@ -6,8 +6,9 @@ defmodule Pipeline.TradePlanner.Impl do
   alias Types.PlannedExecution
   alias Types.TradingSymbol
 
-  @spec plan_execution([TradingSymbol.t()], Decimal.t(), map()) :: PlannedExecution.t()
-  def plan_execution(path, starting_asset_balance, relative_asset_values) do
+  @spec plan_execution([TradingSymbol.t()], Decimal.t(), Decimal.t(), map()) ::
+          PlannedExecution.t()
+  def plan_execution(path, starting_asset_balance, commission_percentage, relative_asset_values) do
     :ok
   end
 end
