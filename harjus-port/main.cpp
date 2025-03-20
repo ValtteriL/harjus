2ca4b3e24c13ac7044d28665106cdbace4cf5665
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-20 09:32:30 +0200

    prevent zombie ports
    Elixir will only close the stdin of the port on exit, thus quit port if stdin is closed.

--- harjus-port/main.cpp
+++ harjus-port/main.cpp
@@ -17,6 +17,13 @@ private slots:
     {
         QTextStream in(stdin);
         QString input = in.readLine();
+
+        // quit if stdin closed
+        if (input.isNull())
+        {
+            QCoreApplication::quit();
+            return;
+        }
         std::cout << "Input received: " << input.toStdString() << std::endl;
         // TODO: process price updates here
     }
