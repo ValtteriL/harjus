83c72cce64431fe0ad3c7ab96c8de8a5a4ab436d
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-20 21:57:17 +0200

    fix catch22 with reference members

--- harjus-port/tradingsymbol.h
+++ harjus-port/tradingsymbol.h
@@ -7,12 +7,12 @@ class TradingSymbol
 public:
   QJsonObject toJson() const;
   static TradingSymbol fromJson(const QJsonObject &json);
-  double price() const { return mPrice; }
+  double price() const { return *mPrice; }
   double qty() const { return mQty; }
   QString symbol() const { return mSymbol; }
-  double maxQty() const { return mMaxQty; }
-  void setPrice(double &price) { mPrice = price; }
-  void setMaxQty(double &qty) { mMaxQty = qty; }
+  double maxQty() const { return *mMaxQty; }
+  void setPrice(double &price) { mPrice = &price; }
+  void setMaxQty(double &qty) { mMaxQty = &qty; }
   double setQtyUpto(double qty); // TODO: take into account minNotional, increments, precisions. Return the actual qty set.
   Position position() const { return mPosition; }
   QString usedAsset() const { return mPosition == Position::LONG ? quote_asset : base_asset; }
@@ -27,7 +27,7 @@ private:
   double mQuoteAssetIncrement;
   int mQuoteAssetPrecision;
   double mMinNotional;
-  double &mPrice;
+  double *mPrice;
   double mQty;
-  double &mMaxQty;
+  double *mMaxQty;
 };
