ab8a696a39009565f46f15316cea74e891ce3e3f
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-21 10:08:14 +0200

    add required includes for tradingsymbol fun reference

---
+++ harjus-port/tradingsymbol.cpp
@@ -0,0 +1,6 @@
+#include "tradingsymbol.h"
+
+TradingSymbol TradingSymbol::fromJson(const QJsonObject &json)
+{
+  return TradingSymbol();
+}
--- harjus-port/tradingsymbol.h
+++ harjus-port/tradingsymbol.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <QString>
 #include <QJsonObject>
 #include "position.h"
