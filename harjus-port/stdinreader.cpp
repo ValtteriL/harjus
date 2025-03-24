0bc6cf9c07b6f0695fae62a538ffd96200bff1f6
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-24 09:44:47 +0200

    add price update to event loop

--- harjus-port/stdinreader.cpp
+++ harjus-port/stdinreader.cpp
@@ -27,6 +27,20 @@ void StdinReader::handleReadyRead()
     QCoreApplication::quit();
     return;
   }
-  std::cout << "Input received: " << input.toStdString() << std::endl;
-  // TODO: process price updates here
+
+  // handle price update
+  auto json = QJsonDocument::fromJson(input.toUtf8()).object();
+  auto opportunities = m_engine.priceUpdate(json);
+
+  if (opportunities.size() > 0)
+  {
+    QJsonArray arr;
+    for (const auto &opportunity : opportunities)
+    {
+      arr.append(opportunity.toJson());
+    }
+
+    // print resulting array of opportunities in single line
+    std::cout << QJsonDocument(arr).toJson(QJsonDocument::Compact).toStdString() << std::endl;
+  }
 };
