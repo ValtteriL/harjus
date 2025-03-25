07a2d136c358deaf58c270aa5f0b155e7b3eb235
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-25 20:14:26 +0200

    remove event loop
    not needed, plus it caused slowdown and invalid results as event queuing did not work as intended

--- harjus-port/main.cpp
+++ harjus-port/main.cpp
@@ -1,13 +1,45 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <string>
 #include <QSocketNotifier>
 #include <QTextStream>
-#include "stdinreader.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include "engine.h"
 
-int main(int argc, char *argv[])
+QJsonObject readJson()
 {
-    QCoreApplication a{argc, argv};
-    StdinReader reader;
+    QTextStream in(stdin);
+    QString input = in.readLine();
+    return QJsonDocument::fromJson(input.toUtf8()).object();
+}
+
+int main()
+{
+    auto engine = Engine::fromJson(readJson());
     std::cout << "ready" << std::endl;
-    return a.exec();
+
+    QTextStream in(stdin);
+    QString input;
+
+    while (!(input = in.readLine()).isNull())
+    {
+        // handle price update
+        auto json = QJsonDocument::fromJson(input.toUtf8()).object();
+        auto opportunities = engine.priceUpdate(json);
+
+        if (opportunities.size() > 0)
+        {
+            QJsonArray arr;
+            for (const auto &opportunity : opportunities)
+            {
+                arr.append(opportunity.toJson());
+            }
+
+            // print resulting array of opportunities in single line
+            std::cout << QJsonDocument(arr).toJson(QJsonDocument::Compact).toStdString() << std::endl;
+        }
+    }
+
+    return 0;
 }
