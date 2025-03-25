457bc379ee5bfc228bfe21569e620213e0bb1064
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-25 20:47:09 +0200

    add harjus-port tests to quality

--- mix.exs
+++ mix.exs
@@ -18,6 +18,8 @@ defmodule Harjus.MixProject do
           "format",
           "credo --strict",
           "compile --all-warnings --warnings-as-errors",
+          "cmd cmake --build build --config Debug --target all --",
+          "cmd ctest -j10 -C Debug -T test --output-on-failure --test-dir build/",
           "test --no-start --include integration:true",
           "credo --strict",
           "dialyzer --ignore-exit-status"
@@ -26,6 +28,8 @@ defmodule Harjus.MixProject do
           "format --check-formatted",
           "credo --strict",
           "compile --all-warnings --warnings-as-errors",
+          "cmd cmake --build build --config Debug --target all --",
+          "cmd ctest -j10 -C Debug -T test --output-on-failure --test-dir build/",
           "test --no-start --include integration:true",
           "credo --strict",
           "dialyzer"
