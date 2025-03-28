6bb0417f04e42256f6c1a5e23f327b004cfc04cc
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-03-28 14:16:24 +0200

    update default path to port
    For some reason /harjus-port fails even though the binary is at / and is called harjus-port. Just `harjus-port` succeeds.

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,7 +26,7 @@ config :harjus,
   balance_exchange: ConfigHelper.get_env("BALANCE_EXCHANGE", Balance.Exchange.Mock, :module),
   console_telemetry: ConfigHelper.get_env("CONSOLE_TELEMETRY", false, :bool),
   console_silence: ConfigHelper.get_env("CONSOLE_SILENCE", false, :bool),
-  path_to_port: ConfigHelper.get_env("PATH_TO_PORT", "/harjus-port", :str)
+  path_to_port: ConfigHelper.get_env("PATH_TO_PORT", "harjus-port", :str)
 
 # configure logger verbosity
 config :logger, level: ConfigHelper.get_env("VERBOSITY", :debug, :atom)
