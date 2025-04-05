af907be16f376399127f2a2b8ed42b130b546f46
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-04-05 10:33:11 +0300

    WIP: start exchange interface
    for exchange related functionality

---
+++ harjus-port/include/Exchange.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Balance.h"
+
+Balance getBalance();
---
+++ harjus-port/src/Exchange.cpp
@@ -0,0 +1,18 @@
+#include "Exchange.h"
+
+Balance getBalance()
+{
+  Balance balance;
+
+  // fetch balance from exchange API
+  // TODO
+
+  // Fill balance object
+  // TODOs
+
+  return std::move(balance);
+}
+
+std::string getBalancesJson(std::string_view uri)
+{
+}
