a8414a6cc9cdc130080e784b061dc80bc1a5cfcc
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-04-07 21:51:27 +0300

    WIP: add Ed25519 signing function

--- docs/binance.md
+++ docs/binance.md
@@ -5,4 +5,7 @@
 ```bash
 openssl genpkey -algorithm ed25519 > $path_to_privkey
 openssl pkey -in $path_to_privkey -pubout > $path_to_pubkey
+
+# new for Harjus 2.0.0
+openssl genpkey -algorithm ed25519 -out private.pem && openssl pkey -in private.pem -pubout && openssl asn1parse -inform PEM -in private.pem -noout -out seed.bin && base64 seed.bin
 ```
---
+++ harjus-port/include/Ed25519.h
@@ -0,0 +1,28 @@
+#include <string>
+
+namespace Ed25519
+{
+
+  /**
+   * @brief Sign a message using the private key
+   * @param message The message to sign
+   * @param privateKey The private key to use for signing
+   * @return The base64 encoded signature
+   */
+  std::string sign(std::string privateKey, std::string payload);
+
+  /**
+   * @brief Base64 decode a string
+   * @param encoded The base64 encoded string
+   * @return The decoded string
+   */
+  std::string base64_decode(const std::string encoded);
+
+  /**
+   * @brief Base64 encode a string
+   * @param data The data to encode
+   * @param length The length of the data
+   * @return The base64 encoded string
+   */
+  std::string base64_encode(const unsigned char *data, size_t length);
+}
