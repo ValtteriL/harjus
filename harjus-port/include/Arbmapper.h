f2e848f02b4123f577d4a1db1153ce3ab6ccbc4a
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-04-09 20:55:32 +0300

    WIP: add arbmapper

--- harjus-port/include/Arbmapper.h
+++ harjus-port/include/Arbmapper.h
@@ -2,5 +2,11 @@
 
 #include <vector>
 #include "Symbol.h"
+#include "ITrade.h"
 
-std::vector<std::vector<Symbol>> getTradingPaths(std::vector<Symbol> &symbols, int maxDepth, std::vector<Symbol> &skipSymbols);
+/**
+ * @brief Get trading paths.
+ * Given a map of , find all possible trading paths within a maximum depth.
+ * The function will skip any paths starting with symbols that are in the skipSymbols vector.
+ */
+std::vector<std::vector<ITrade>> getTradingPaths(std::unordered_map<std::string, Symbol> *symbolMap, int maxDepth, std::vector<std::string> &skipSymbols);
--- harjus-port/src/Arbmapper.cpp
+++ harjus-port/src/Arbmapper.cpp
@@ -1,6 +1,68 @@
 #include "Arbmapper.h"
+#include <boost/graph/adjacency_list.hpp>
 
-std::vector<std::vector<Symbol>> getTradingPaths(std::vector<Symbol> &symbols, int maxDepth, std::vector<Symbol> &skipSymbols)
+struct VertexProperties
 {
-  return std::vector<std::vector<Symbol>>();
+  std::string asset;
+};
+struct EdgeProperties
+{
+  ITrade trade;
+};
+
+// Define the graph structure
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexProperties, EdgeProperties> Graph;
+
+// Function to build the graph
+void buildGraph(Graph &graph, const std::unordered_map<std::string, Symbol> *symbolMap)
+{
+
+  // Create a map to store vertexes by asset
+  std::unordered_map<std::string, std::size_t> vertexMap;
+
+  // get unique asset names
+  std::unordered_set<std::string> uniqueAssets;
+  for (const auto &pair : *symbolMap)
+  {
+    const auto &symbol = pair.second;
+    uniqueAssets.insert(symbol.baseAsset);
+    uniqueAssets.insert(symbol.quoteAsset);
+  }
+
+  // Add vertices to the graph
+  for (const auto &asset : uniqueAssets)
+  {
+    auto vertex = boost::add_vertex(VertexProperties{asset}, graph);
+
+    // insert the vertex into the map
+    vertexMap[asset] = vertex;
+  }
+
+  // Add edges to the graph
+  for (const auto &pair : *symbolMap)
+  {
+    const auto &symbol = pair.second;
+
+    auto vertex1 = vertexMap[symbol.baseAsset];
+    auto vertex2 = vertexMap[symbol.quoteAsset];
+
+    // long
+    // TODO: construct the trade object
+    boost::add_edge(vertex1, vertex2, EdgeProperties{}, graph);
+
+    // short
+    // TODO: construct the trade object
+    boost::add_edge(vertex2, vertex1, EdgeProperties{}, graph);
+  }
+}
+
+std::vector<std::vector<ITrade>> getTradingPaths(std::unordered_map<std::string, Symbol> *symbolMap, int maxDepth, std::vector<std::string> &skipSymbols)
+{
+  // Create a graph
+  Graph graph;
+
+  // Build the graph with the given symbols
+  buildGraph(graph, symbolMap);
+
+  // TODO: find & return cycles in the graph
 }
