b2af81a123809bcb8b56b9c5cd42d60fbe5403bf
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-04-10 10:16:52 +0300

    make test more explicit

--- harjus-port/tests/ArbmapperTest.cpp
+++ harjus-port/tests/ArbmapperTest.cpp
@@ -22,25 +22,41 @@ TEST(ArbmapperTest, detectsAllTradingOpportunities)
 {
   // Create a vector of symbols
   std::unordered_map<std::string, Symbol> symbolMap{
-      {"BTCUSDT", {"BTCUSDT", "BTC", "USDT", 30000, 30010, 1, 1, 10, 0.0001, 0.01, 8, 2}},
-      {"ETHBTC", {"ETHBTC", "ETH", "BTC", 0.07, 0.071, 1, 1, 10, 0.0001, 0.01, 8, 8}},
-      {"USDTETH", {"USDTETH", "USDT", "ETH", 1400, 1410, 1, 1, 10, 0.0001, 0.01, 8, 8}}};
+      {"BTCETH", {"BTCETH", "BTC", "ETH", 30000, 30010, 1, 1, 10, 0.0001, 0.01, 8, 2}},
+      {"ETHDOGE", {"ETHDOGE", "ETH", "DOGE", 0.07, 0.071, 1, 1, 10, 0.0001, 0.01, 8, 8}},
+      {"DOGEBTC", {"DOGEBTC", "DOGE", "BTC", 1400, 1410, 1, 1, 10, 0.0001, 0.01, 8, 8}}};
 
   int depth = 3;
   std::vector<std::string> skipSymbols{};
   auto opportunities = getTradingPaths(&symbolMap, depth, skipSymbols);
 
-  // Verify that opportunities are detected
-  EXPECT_FALSE(opportunities.empty());
+  // Verify that 6 opportunities are detected
+  EXPECT_EQ(opportunities.size(), 6);
 
-  // Verify that all paths are within the depth limit
+  // Verify that all paths are of length 3
   for (const auto &path : opportunities)
   {
-    EXPECT_LE(path.size(), depth);
+    EXPECT_EQ(path.size(), 3);
   }
 
+  // Verify that no trades are the same object (their addresses are different)
+  int totalTrades = 0;
+  for (const auto &path : opportunities)
+  {
+    totalTrades += path.size();
+  }
+  std::unordered_set<std::string> uniqueTradeAddresses;
+  for (const auto &path : opportunities)
+  {
+    for (const auto &trade : path)
+    {
+      uniqueTradeAddresses.insert(std::to_string(reinterpret_cast<std::uintptr_t>(&trade)));
+    }
+  }
+  EXPECT_EQ(uniqueTradeAddresses.size(), totalTrades);
+
   // Verify that the returned trading paths contain at least one path
-  // with each of the following as the first usedCurrency: BTC, ETH, USDT
+  // with each of the following as the first usedCurrency: BTC, ETH, DOGE
   std::unordered_set<std::string> firstUsedCurrencies;
   for (const auto &path : opportunities)
   {
@@ -48,5 +64,5 @@ TEST(ArbmapperTest, detectsAllTradingOpportunities)
   }
   EXPECT_TRUE(firstUsedCurrencies.count("BTC"));
   EXPECT_TRUE(firstUsedCurrencies.count("ETH"));
-  EXPECT_TRUE(firstUsedCurrencies.count("USDT"));
+  EXPECT_TRUE(firstUsedCurrencies.count("DOGE"));
 }
