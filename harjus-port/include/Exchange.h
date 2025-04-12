bf0270133e87762b070f6759d67af7f622446015
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-04-12 17:45:11 +0300

    implement Exchange::getRelativeValues

--- harjus-port/include/Exchange.h
+++ harjus-port/include/Exchange.h
@@ -4,6 +4,8 @@
 #include "IConfiguration.h"
 #include "Symbol.h"
 #include <unordered_map>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
 Balance getBalance(IConfiguration &config);
 std::unordered_map<std::string, Symbol> getSymbols(IConfiguration &config);
+std::unordered_map<std::string, boost::multiprecision::cpp_dec_float_50> getRelativeValues(IConfiguration &config, const std::unordered_map<std::string, Symbol> &symbols);
