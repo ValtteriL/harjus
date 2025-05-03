43ecd80231fd0f9d120b9cfe48d08d3e2f95ca68
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-05-03 21:23:04 +0300

    implement ExecutionReport

--- include/ExecutionReport.h
+++ include/ExecutionReport.h
@@ -1,11 +1,53 @@
 #pragma once
 
+#include "TradeExecutionStatus.h"
+#include <boost/multiprecision/cpp_dec_float.hpp>
 #include <string>
+#include <unordered_map>
+
+/**
+ * @brief Class for execution reports from the exchange
+ */
 class ExecutionReport {
 private:
   std::string _id;
+  TradeExecutionStatus _status;
+  std::unordered_map<std::string, boost::multiprecision::cpp_dec_float_50>
+      _assetDelta;
 
 public:
-  ExecutionReport(const std::string &id);
+  /**
+   * @brief Constructor for ExecutionReport class
+   * @param id The ID of the execution report
+   * @param status The status of the execution report
+   * @param assetDelta The asset delta of the execution report
+   */
+  ExecutionReport(
+      const std::string &id, TradeExecutionStatus status,
+      const std::unordered_map<
+          std::string, boost::multiprecision::cpp_dec_float_50> &assetDelta);
+
+  /**
+   * @brief Destructor for ExecutionReport class
+   */
   virtual ~ExecutionReport() = default;
+
+  /**
+   * @brief Get the ID of the execution report
+   * @return The ID of the execution report
+   */
+  std::string getId() const;
+
+  /**
+   * @brief Get the status of the execution report
+   * @return The status of the execution report
+   */
+  TradeExecutionStatus getStatus() const;
+
+  /**
+   * @brief Get the asset delta of the execution report
+   * @return The asset delta of the execution report
+   */
+  std::unordered_map<std::string, boost::multiprecision::cpp_dec_float_50>
+  getAssetDelta() const;
 };
--- src/ExecutionReport.cpp
+++ src/ExecutionReport.cpp
@@ -1,3 +1,16 @@
 #include "ExecutionReport.h"
 
-ExecutionReport::ExecutionReport(const std::string &id) : _id(id) {}
+ExecutionReport::ExecutionReport(
+    const std::string &id, TradeExecutionStatus status,
+    const std::unordered_map<
+        std::string, boost::multiprecision::cpp_dec_float_50> &assetDelta)
+    : _id(id), _status(status), _assetDelta(assetDelta) {};
+
+std::string ExecutionReport::getId() const { return _id; }
+
+TradeExecutionStatus ExecutionReport::getStatus() const { return _status; }
+
+std::unordered_map<std::string, boost::multiprecision::cpp_dec_float_50>
+ExecutionReport::getAssetDelta() const {
+  return _assetDelta;
+}
