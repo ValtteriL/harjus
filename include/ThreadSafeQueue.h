ddc600a62996e07863b358496f4856788dc4a62b
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-05-07 10:33:57 +0300

    replace condition variable with semaphore

--- include/ThreadSafeQueue.h
+++ include/ThreadSafeQueue.h
@@ -3,47 +3,58 @@
 /**
  * a thread-safe queue implementation
  */
-#include <condition_variable>
 #include <deque>
 #include <mutex>
+#include <semaphore>
 template <typename T> class ThreadSafeQueue {
 
 private:
-  std::deque<T> deque;
-  std::mutex mutex;
-  std::condition_variable condNotEmpty;
-
-public:
-  /*
-   * @brief Push an element to the queue
-   * @param value The value to push
-   */
-  void push(const T &value) {
-    std::unique_lock<std::mutex> lock(mutex);
-    deque.push_back(value);
-    lock.unlock();
-    // Notify one waiting thread that an item has been added
-    condNotEmpty.notify_one();
-  }
-
-  /*
-   * @brief Pop an element from the queue
-   * @return The popped value
-   */
-  T pop() {
-    std::unique_lock<std::mutex> lock(mutex);
-    condNotEmpty.wait(lock, [this] { return !deque.empty(); });
-    T value = deque.front();
-    deque.pop_front();
-    return value;
-  }
+  std::deque<T> _deque;
+  std::mutex _mutex;
+  std::binary_semaphore &_semaphore;
 
   /*
    * @brief Check if the queue is empty
    * @return True if the queue is empty, false otherwise
    */
   bool empty() {
-    std::lock_guard<std::mutex> lock(mutex);
-    return deque.empty();
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _deque.empty();
   }
+
+public:
+  /*
+   * @brief Constructor
+   * @param semaphore The semaphore to use for synchronization
+   */
+  ThreadSafeQueue(std::binary_semaphore &semaphore) : _semaphore(semaphore) {}
+  /*
+   * @brief Push an element to the queue
+   * @param value The value to push
+   */
+  void push(const T &value) {
+    std::lock_guard<std::mutex> lock(_mutex);
+    _deque.push_back(value);
+    _semaphore.release();
+  }
+
+  /*
+   * @brief Pop an element from the queue
+   * @return The popped value
+   */
+  bool try_pop(T &item) {
+    std::lock_guard<std::mutex> lock(_mutex);
+    if (_deque.empty()) {
+      return false;
+    }
+    item = std::move(_deque.front());
+    _deque.pop_front();
+    return true;
+  }
+
+  /*
+   * @brief Get the semaphore
+   * @return The semaphore
+   */
+  std::binary_semaphore &getSemaphore() { return _semaphore; }
 };
