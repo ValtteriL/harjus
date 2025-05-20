eeb1a9904ba227d2612bbf62b586ea3c9d058f12
Valtteri <valtteri@shufflingbytes.com>
2025-05-20 09:35:14 +0300

    Use alpine base image
    The default scratch image lacks temp directory

--- default.nix
+++ default.nix
@@ -104,6 +104,14 @@ let
     # docker packaging derivation
     docker = pkgs.dockerTools.buildLayeredImage {
       name = "harjus";
+      fromImage = pkgs.dockerTools.pullImage {
+        imageName = "library/alpine";
+        imageDigest =
+          "sha256:1c4eef651f65e2f7daee7ee785882ac164b02b78fb74503052a26dc061c90474";
+        finalImageName = "alpine";
+        finalImageTag = "3.21.3";
+        sha256 = "sha256-BLd0y9w1FIBJO5o4Nu5Wuv9dtGhgvh+gysULwnR9lOo=";
+      };
       created = "now";
       config = {
         Cmd = [ "harjus" ];
