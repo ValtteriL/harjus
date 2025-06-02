dd088efe0d82138ea4d4c4af1524e5fc44205ce6
Valtteri <valtteri@shufflingbytes.com>
2025-06-02 20:57:54 +0300

    Reconnect after 1 second
    Minimize window of expired trade information just in case a session disconnects and somewhy Harjus does not stop.

--- src/FixConfig.cpp
+++ src/FixConfig.cpp
@@ -817,6 +817,7 @@ FixConfig::FixConfig(const IConfiguration &config) {
   HeartBtInt=30
   ResetOnLogout=Y
   ResetOnDisconnect=Y
+  ReconnectInterval=1
   FileStorePath=)" + config.getFixFileStorePath() +
                            R"(
   ConnectionType=initiator
