3638e808e47cb7c2ac433c4ccda0358b24ba4354
Valtteri <valtteri@shufflingbytes.com>
2025-06-03 10:01:56 +0300

    express intent

--- src/Balance.cpp
+++ src/Balance.cpp
@@ -2,29 +2,18 @@
 
 void Balance::updateBalance(const std::string &currency, PreciseNumber amount) {
   std::unique_lock<std::shared_mutex> lock(mtx);
-  if (balanceMap.find(currency) != balanceMap.end()) {
-    balanceMap[currency] += amount;
-  } else {
-    balanceMap[currency] = amount;
-  }
+  balanceMap[currency] += amount;
 }
 
 void Balance::updateBalance(
     const std::unordered_map<std::string, PreciseNumber> &assetDelta) {
   std::unique_lock<std::shared_mutex> lock(mtx);
-  for (const auto &[currency, amount] : assetDelta) {
-    if (balanceMap.find(currency) != balanceMap.end()) {
-      balanceMap[currency] += amount;
-    } else {
-      balanceMap[currency] = amount;
-    }
-  }
+  std::for_each(assetDelta.begin(), assetDelta.end(), [this](const auto &pair) {
+    balanceMap[pair.first] += pair.second;
+  });
 }
 
 PreciseNumber Balance::getBalance(const std::string &currency) {
   std::shared_lock<std::shared_mutex> lock(mtx);
-  if (balanceMap.find(currency) != balanceMap.end()) {
-    return balanceMap.at(currency);
-  }
-  return PreciseNumber{"0"};
+  return balanceMap[currency];
 }
