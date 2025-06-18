da3ce1abe2e947f60126d73a3942b2d2d265ad89
Valtteri <valtteri@shufflingbytes.com>
2025-06-18 09:40:42 +0300

    make pname, version overridable with --argstr
    like so:
    `nix-build -A harjus --argstr version 1.0.0`

--- default.nix
+++ default.nix
@@ -2,14 +2,11 @@
   url =
     "https://github.com/NixOS/nixpkgs/archive/3f0a8ac25fb674611b98089ca3a5dd6480175751.tar.gz";
   sha256 = "sha256:10i7fllqjzq171afzhdf2d9r1pk9irvmq5n55h92rc47vlaabvr4";
-}) { config.allowUnfree = true; } }:
+}) { config.allowUnfree = true; }, pname ? "harjus", version ? "latest" }:
 
 with pkgs;
 
 let
-
-  version = "1.0.0";
-  pname = "harjus";
 
   gccFlags =
     "-O3 -march=icelake-server -mtune=icelake-server -pipe -fsemantic-interposition";
