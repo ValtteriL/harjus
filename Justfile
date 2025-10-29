cf918e64607ecd9ddb91ad1c66e0c2811e2cca5a
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-10-29 14:30:39 +0200

    Finish fstack packaging
    Using libdpdk 22.11.1, as its closest to the bundled one (22.11.6)
    It may not be compatible with the version installed on host (23.11.4 on Ubuntu 24.04).

--- Justfile
+++ Justfile
@@ -5,5 +5,4 @@ install-deps:
     sudo apt install dpdk dpdk-doc dpdk-kmods-dkms dpdk-dev libdpdk-dev
 
 build-fstack:
-    cd lib \
-    && PKG_CONFIG_PATH_FOR_TARGET=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH_FOR_TARGET make
+    nix-build -A fstack
--- fstack.nix
+++ fstack.nix
@@ -18,6 +18,12 @@ let
     rev = "1.24";
     sha256 = "sha256-zsIOQ03q/33VPME12CnMOo79xZRIpETrcZwuEMwyXQ8=";
   };
+
+  # commit where dpdk is at version 22.11.1 (close to the f-stack 1.24 supported 22.11.6)
+  pinnedNixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/203e5461b25add434893bee7ba8bdfeeffffebf0.tar.gz";
+    sha256 = "sha256:1b5gcaxl461pbiawcyqh5zh6smlp25sbm00d9cla9il4rd928jyp";
+  }) {};
 in
 
 stdenv.mkDerivation {
@@ -29,5 +35,15 @@ stdenv.mkDerivation {
   sourceRoot = "${src.name}/lib";
 
   nativeBuildInputs = [ pkg-config gawk ];
-  buildInputs = [ openssl numactl pcre zlib bc ];
+  buildInputs = [ openssl numactl pcre zlib bc pinnedNixpkgs.dpdk ];
+
+  # Install into the Nix store: create directories and call make install with
+  # PREFIX variables so the Makefile writes into $out instead of /usr/local.
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out/lib" "$out/include" "$out/bin" "$out/etc"
+    make install PREFIX_LIB=$out/lib PREFIX_INCLUDE=$out/include \
+      PREFIX_BIN=$out/bin F-STACK_CONF=$out/etc/f-stack.conf
+    runHook postInstall
+  '';
 }
