12343992a9c88eaab46cbb04458d22ea107ff6f7
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-11-09 19:08:11 +0000

    Upgrade to fstack 1.25
    Upgrade dpdk to 23.11 in lockstep

--- fstack-examples.nix
+++ fstack-examples.nix
@@ -5,20 +5,20 @@ let
   src = fetchFromGitHub {
     owner = "F-Stack";
     repo = "f-stack";
-    rev = "1.24";
-    sha256 = "sha256-zsIOQ03q/33VPME12CnMOo79xZRIpETrcZwuEMwyXQ8=";
+    rev = "v1.25";
+    sha256 = "sha256-k7GEg3oAr/1qQcXBa6ABIGv4cQ9/i8IRqoGR7AGVLbM=";
   };
 
-  # commit where dpdk is at version 22.11.1 (close to the f-stack 1.24 supported 22.11.6)
+  # commit where dpdk is at version 23.11 (close to the f-stack 1.25 supported 23.11.5)
   pinnedNixpkgs = import (fetchTarball {
     url =
-      "https://github.com/NixOS/nixpkgs/archive/203e5461b25add434893bee7ba8bdfeeffffebf0.tar.gz";
-    sha256 = "sha256:1b5gcaxl461pbiawcyqh5zh6smlp25sbm00d9cla9il4rd928jyp";
+      "https://github.com/NixOS/nixpkgs/archive/571c71e6f73af34a229414f51585738894211408.tar.gz";
+    sha256 = "sha256:0fgp5sqfmh5zgx75rs5101ywkz0fkjff67abms0kc8hyaxmlc7js";
   }) { };
 
 in stdenv.mkDerivation {
   pname = "fstack-examples";
-  version = "1.24";
+  version = "1.25";
 
   inherit src;
 
