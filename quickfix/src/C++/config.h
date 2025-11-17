355e0641c870dcb99204fcd696c207ab9cdf88e9
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-11-17 20:49:51 +0000

    Track cmake files

--- quickfix/src/C++/config.h
+++ quickfix/src/C++/config.h
@@ -1,10 +1,4 @@
-#ifndef CONFIG_H_IN
-#define CONFIG_H_IN
+#pragma once
 
 #define HAVE_CXX17
 /* #undef HAVE_STD_SHARED_PTR */
-/* #undef HAVE_MYSQL */
-/* #undef HAVE_POSTGRESQL */
-#define HAVE_SSL 1
-
-#endif
