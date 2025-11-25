681e5f16a6f524f05509e5df05fc78b82e418663
Valtteri <valtteri@shufflingbytes.com>
2025-11-25 21:23:21 +0200

    Finish package flashfix with conan

--- flashfix/test_package/src/example.cpp
+++ flashfix/test_package/src/example.cpp
@@ -1,7 +1,9 @@
 #include <FstackMicroThreadedSSLSocketInitiator.h>
 #include <MessageStore.h>
+#include <print>
 
 int main()
 {
     FIX::MemoryStoreFactory storeFactory{};
+    std::println("Flashfix tests passed.");
 }
