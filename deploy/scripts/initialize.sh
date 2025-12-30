4565bebc30e60367b514ea7a81cd53acb8c41e0e
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2025-12-30 22:37:20 +0200

    Configure vagrantfile
    - privisioning
    - mount project
    - 1 bridged NIC for DPDK
    - initialization
    - command to run debug build

--- deploy/scripts/initialize.sh
+++ deploy/scripts/initialize.sh
@@ -4,7 +4,7 @@ set -x
 
 # prepare host for kernel bypass via f-stack
 
-# set hugepage	
+# Set hugepages
 echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 mkdir -p /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
@@ -12,19 +12,20 @@ mount -t hugetlbfs nodev /mnt/huge
 # close ASLR; it is necessary in multiple process
 echo 0 > /proc/sys/kernel/randomize_va_space
 
-# insmod ko
+# load kernel modules
 modprobe uio
 modprobe hwmon
 modprobe igb_uio
 
-# take over ens19 for f-stack
-if [ -d /sys/class/net/ens19 ]; then
-    ifconfig ens19 down
-    dpdk-devbind.py --bind=igb_uio ens19
+# take over enp0s8 for f-stack
+if [ -d /sys/class/net/enp0s8 ]; then
+    ifconfig enp0s8 down
+    dpdk-devbind.py --bind=igb_uio enp0s8
+
+    # to undo, run:
+    # dpdk-devbind.py --bind=virtio-pci 00:03.0
+    # ifconfig enp0s8 up
 fi
-# to undo, run:
-# dpdk-devbind.py --bind=virtio-pci 00:13.0
-# ifconfig ens19 up
 
 # show status
 dpdk-devbind.py --status
