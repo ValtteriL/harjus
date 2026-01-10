8d8c2d1f7c4e0542e7b55940f1c7055d371406e1
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2026-01-10 20:33:03 +0000

    Add runtime dependency
    Headers required for igb_uio module

--- deploy/scripts/provision-runtime.sh
+++ deploy/scripts/provision-runtime.sh
@@ -6,6 +6,7 @@ set -e
 # Install runtime dependencies
 sudo apt-get update && \
     sudo apt-get install -y --no-install-recommends \
+    linux-headers-$(uname -r) \
     dpdk \
     dpdk-doc \
     dpdk-kmods-dkms \
