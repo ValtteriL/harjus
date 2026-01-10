b68b68617e35d52ed30e9dd0fa0599056d0b18a5
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2026-01-10 18:29:37 +0000

    Cleanup unused files

--- flashfix/test_package/src/example.cpp
+++
@@ -1,9 +0,0 @@
-#include <FstackMicroThreadedSSLSocketInitiator.h>
-#include <MessageStore.h>
-#include <print>
-
-int main()
-{
-    FIX::MemoryStoreFactory storeFactory{};
-    std::println("Flashfix tests passed.");
-}
